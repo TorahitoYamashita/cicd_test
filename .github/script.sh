20d5791b41498517a4e99dab89f4f6eb18e0664e
TorahitoYamashita <torahitoyamashita@Torahitos-MacBook-Air.local>
2022-07-20 15:10:53 -0400

    a

--- .github/script.sh
+++ .github/script.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
-pwd
-ls
 
-cat "./manifest.txt"
-YEAR_AND_WEEK="$(date +"%y.%U.")"
+set -ex
 
-IFS='.' read -ra ADDR <<< $1
-echo ${ADDR[2]}
+if [ -z "$1" ]; then
+    echo 'ERROR: Missing input variable!'
+    exit 1
+fi
 
-YEAR="$(date +"%y")"
+YEAR_AND_WEEK="v$(date +"%y.%U.")"
 WEEK="$(date +"%U")"
 
-if [ "$WEEK" = "${ADDR[2]}" ]; then
-    echo "Same week."
-    VERSION=${YEAR_AND_WEEK}$((${ADDR[2]} + 1))
+IFS='.' read -ra ARRAY <<< $1
+
+if [ "$WEEK" = "${ARRAY[2]}" ]; then
+    VERSION=${YEAR_AND_WEEK}$((${ARRAY[2]} + 1))
 else
-    echo "Next week."
     VERSION=${YEAR_AND_WEEK}1
 fi
 
-echo ${VERSION}
-
-echo "before 1"
-cat "./manifest.txt"
-echo "after 1"
-
 echo ${VERSION} > "./manifest.txt"
-
-echo "before 2"
-cat "./manifest.txt"
-echo "after 2"
